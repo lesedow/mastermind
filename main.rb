30ee6bfc6c337de66f57600792e0a15196461368
lesedow <lesedow@gmail.com>
2021-12-30 18:03:48 +0200

    extracted code in multiple files

--- main.rb
+++ main.rb
@@ -1,69 +1,18 @@
-class Mastermind
-  COLORS = ['red', "green", 'blue', 'yellow', 'cyan', 'pink', 'purple']
-  
-  def initialize(turns = 12, player_role = 'codebreaker')
-    @turns = turns
-    @player_role = player_role
-  end
+require './mastermind'
 
-  def start
-    @player_role == 'codebreaker' ? play_as_guesser : play_as_maker
-  end
+def main
+  role = gets.chomp
 
-  def play_as_guesser
-    # Make the computer select 4 random colors
-    @computer_selection = random_selection
-    puts "The computer has choosen 4 random colors. You have #{@turns} turns available to guess them."
-   
-    # The player will have n number of turns available to guess the colors
-    puts "This is the list of all the available colors"
-    COLORS.each { |color| print "#{color}, "}
-    puts "\nIn order to make a guess, you have to type all of the four colors (ex: blue cyan red green)"
-    until @turns == 0
-      play_turn
-      break if @feedback_status
-      @turns -= 1
-    end
-    conclude    
-    # Each turn the computer will give some feedback about the player's guess
-  end 
-
-  def play_turn
-    player_guess = gets.chomp.split(' ')
-    if player_guess.length != 4
-      puts "You should include all the colors in this manner: blue cyan red green"
-      play_turns
-    end
-    feedback(player_guess)
-  end
-
-  def conclude
-    if @feedback_status
-      puts "Congrats! You have guessed the computer selection of colors."
-      return
-    end
-    puts "You have no more turns left! Sorry."
-  end
-
-  def random_selection
-    COLORS.sample(4)    
-  end
-
-  def feedback(guess)
-    wrong_order = 0
-    correct = 0
-    guess.each_with_index do |color, i|
-      if @computer_selection.include?(color)
-        if @computer_selection[i] == color
-          correct += 1
-        else
-          wrong_order += 1
-        end
-      end
-    end
-    puts "feedback: There are #{wrong_order} colors in the wrong order and #{correct} placed correctly!"
-    correct == 4 ? @feedback_status = true : @feedback_status = false
+  case role
+  when 'codebreaker'
+    Mastermind.new.play_as_codebreaker
+  when 'codemaker'
+    Mastermind.new.play_as_codemaker
+  else
+    puts 'Invalid role, choose again.'
+    main
   end
 end
 
-Mastermind.new.start
+puts 'Welcome to Mastermind! Select your role: codebreaker or codemaker'
+main
